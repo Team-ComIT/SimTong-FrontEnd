79c0fc2c8be369f1392dd16807fa03739c75bc94
김벽걸 <biob1121@naver.com>
2022-08-08 00:57:25 +0900

    chore(main): globalStyle 세팅

--- simtong/src/App.tsx
+++ simtong/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import style from './GolobalStyles';
+import { Global } from '@emotion/react';
 import './App.css';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Global styles={style} />
+        </div>
+    );
 }
 
 export default App;
