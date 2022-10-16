9de42dab8a6d23b7cce1167dd59f97eb22613659
songiwon <biob1121@naver.com>
2022-10-16 22:32:49 +0900

    refactor: 코드 리뷰 반영

--- apis/instance.ts
+++ apis/instance.ts
@@ -3,7 +3,5 @@ import axios from 'axios';
 export const instance = axios.create({
     baseURL: 'https://',
     timeout: 10000,
-    headers: {
-        // Authorization: `Bearer ${cookie.get('access_token')}`,
-    },
+    headers: {},
 });
