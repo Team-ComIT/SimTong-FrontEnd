9a29f8a9a01eb7124bbc4776366734c624288ed9
김벽걸 <biob1121@naver.com>
2022-11-27 13:02:56 +0900

    fix: 병합 오류 해결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 import '../styles/globals.css';
+import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
-import { QueryClientProvider, QueryClient } from 'react-query';
+import { store } from '../store/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const queryClient = new QueryClient();
-
     return (
-        <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
-        </QueryClientProvider>
+        <>
+            <Provider store={store}>
+                <Component {...pageProps} />
+            </Provider>
+        </>
     );
 }
-
 export default MyApp;
